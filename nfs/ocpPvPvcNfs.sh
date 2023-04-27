1293efb0979c36d6dbfa248783d9972ec42bead5
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-04-27 15:27:02 -0300

    Update ocpPvPvcNfs.sh

--- nfs/ocpPvPvcNfs.sh
+++ nfs/ocpPvPvcNfs.sh
@@ -4,12 +4,22 @@ echo $PROJECT
 echo $STORAGECLASSBLOCK
 echo $APICLICENCE
 
-mkdir -p /tmp/$PROJECT/apiconnectInstance
-chmod a+x /tmp/$PROJECT/apiconnectInstance
+mkdir -p /tmp/nfs/$PROJECT/ocpPvPvcNfs
+chmod a+x /tmp/nfs/$PROJECT/ocpPvPvcNfs
 
-cat ~/more-resources/nfs/ocpPvNfs.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" |  sed "s/{###PROVIDE_YOUR_DIR_SHARED_NFS_SERVER_HERE###}/${DIR_SHARED_NFS_SERVER}/g" | sed "s/{###PROVIDE_YOUR_IP_NFS_SERVER_HERE###}/${IP_NFS_SERVER}/g" >/tmp/nfs/ocpPvNfs_OK.yaml
-oc apply -f /tmp/nfs/ocpPvNfs_OK.yaml
+cat ~/more-resources/nfs/ocpPvNfs.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" |  sed "s/{###PROVIDE_YOUR_DIR_SHARED_NFS_SERVER_HERE###}/${DIR_SHARED_NFS_SERVER}/g" | sed "s/{###PROVIDE_YOUR_IP_NFS_SERVER_HERE###}/${IP_NFS_SERVER}/g" >/tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvNfs_OK.yaml
+oc apply -f /tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvNfs_OK.yaml
 
-cat ~/more-resources/nfs/ocpPvcNfs.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" |  sed "s/{###PROVIDE_YOUR_DIR_SHARED_NFS_SERVER_HERE###}/${DIR_SHARED_NFS_SERVER}/g" | sed "s/{###PROVIDE_YOUR_IP_NFS_SERVER_HERE###}/${IP_NFS_SERVER}/g" >/tmp/nfs/ocpPvcNfs_OK.yaml
-oc apply -f /tmp/nfs/ocpPvcNfs_OK.yaml
+cat ~/more-resources/nfs/ocpPvcNfs.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" |  sed "s/{###PROVIDE_YOUR_DIR_SHARED_NFS_SERVER_HERE###}/${DIR_SHARED_NFS_SERVER}/g" | sed "s/{###PROVIDE_YOUR_IP_NFS_SERVER_HERE###}/${IP_NFS_SERVER}/g" >/tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvcNfs_OK.yaml
+oc apply -f /tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvcNfs_OK.yaml
 
+echo "Para deletar: "
+echo "oc delete -f /tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvNfs_OK.yaml"
+echo "oc delete -f /tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvcNfs_OK.yaml"
+echo "Ou execute o script: /tmp/nfs/$PROJECT/ocpPvPvcNfs/./nfs_uninstall.sh"
+
+cat <<EOF > /tmp/nfs/$PROJECT/ocpPvPvcNfs/./nfs_uninstall.sh
+oc delete -f /tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvNfs_OK.yaml
+oc delete -f /tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvcNfs_OK.yaml
+EOF
+chmod 777 /tmp/nfs/$PROJECT/ocpPvPvcNfs/./nfs_uninstall.sh
