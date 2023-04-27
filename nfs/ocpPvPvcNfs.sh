8d38e6af3c24d6fbdb7c464147591049df090b35
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-04-27 15:28:25 -0300

    Update ocpPvPvcNfs.sh

--- nfs/ocpPvPvcNfs.sh
+++ nfs/ocpPvPvcNfs.sh
@@ -10,7 +10,7 @@ chmod a+x /tmp/nfs/$PROJECT/ocpPvPvcNfs
 cat ~/more-resources/nfs/ocpPvNfs.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" |  sed "s/{###PROVIDE_YOUR_DIR_SHARED_NFS_SERVER_HERE###}/${DIR_SHARED_NFS_SERVER}/g" | sed "s/{###PROVIDE_YOUR_IP_NFS_SERVER_HERE###}/${IP_NFS_SERVER}/g" >/tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvNfs_OK.yaml
 oc apply -f /tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvNfs_OK.yaml
 
-cat ~/more-resources/nfs/ocpPvcNfs.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" |  sed "s/{###PROVIDE_YOUR_DIR_SHARED_NFS_SERVER_HERE###}/${DIR_SHARED_NFS_SERVER}/g" | sed "s/{###PROVIDE_YOUR_IP_NFS_SERVER_HERE###}/${IP_NFS_SERVER}/g" >/tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvcNfs_OK.yaml
+cat ~/more-resources/nfs/ocpPvcNfs.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g"  >/tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvcNfs_OK.yaml
 oc apply -f /tmp/nfs/$PROJECT/ocpPvPvcNfs/ocpPvcNfs_OK.yaml
 
 echo "Para deletar: "
